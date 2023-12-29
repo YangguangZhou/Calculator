9931f7409e81520b1a65e0be13b822089062f9e1
Jerry Zhou <61733195+YangguangZhou@users.noreply.github.com>
2023-12-29 22:42:33 +0800

    更新 script.js

--- crazy/script.js
+++ crazy/script.js
@@ -1,19 +1,31 @@
+function isNumber(n) {
+  return !isNaN(parseFloat(n)) && isFinite(n);
+}
+
 function convertToChinese(num) {
   const chineseNum = ['一', '二', '三', '四', '五', '六', '七', '八', '九'];
-  return chineseNum[num - 1];
+  
+  // Check if num is within 1-9
+  if(num >= 1 && num <= 9) {
+    return chineseNum[num - 1];
+  } else {
+    return num;
+  }
 }
 
 function calculate() {
   const number = document.getElementById('number').value;
-  let result = 4/50 * number;
+  
+  if (!isNumber(number)) {
+    document.getElementById('result').textContent = "输入错误";
+    return;
+  };
 
-  result = Math.round(result * 100) / 100; // Rounding to 2 decimal places
+  let result = (4 * number) / 50;
 
-  if (result >= 1 && result <= 9) {
-    result = `今天是疯狂星期${convertToChinese(result)}，v我${number}`;
-  } else {
-    result = `今天是疯狂星期${result}，v我${number}`;
-  }
+  result = Math.round(result * 100) / 100;
+
+  result = `今天是疯狂星期${convertToChinese(result)}，v我${number}`;
 
   document.getElementById('result').textContent = result;
 }
